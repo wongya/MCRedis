e39eac6d9509b6501008d16b519d258abda15a7d
wongya <wongya@gmail.com>
2016-05-02 22:06:32 +0900

    hiredis for vc2013

---
+++ lib/hiredis/build_vs12.bat
@@ -0,0 +1,12 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+
+cl /D "WIN32" /c /MTd /Zi /Fd"hiredis.pdb" async.c
+cl /D "WIN32" /c /MTd /Zi /Fd"hiredis.pdb" dict.c
+cl /D "WIN32" /c /MTd /Zi /Fd"hiredis.pdb" hiredis.c
+cl /D "WIN32" /c /MTd /Zi /Fd"hiredis.pdb" net.c
+cl /D "WIN32" /c /MTd /Zi /Fd"hiredis.pdb" read.c
+cl /D "WIN32" /c /MTd /Zi /Fd"hiredis.pdb" sds.c
+
+lib async.obj dict.obj hiredis.obj net.obj read.obj sds.obj /OUT:hiredis_vs12.lib /MACHINE:X64
